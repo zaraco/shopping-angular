7d17ce33d213d568227e812dd7079ab14e4791fe
Zahra Teymouri <zteymouri@red.software.systems>
2022-01-02 15:26:21 +0100

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './component/cart/cart.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
