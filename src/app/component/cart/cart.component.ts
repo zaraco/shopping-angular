2dcdb04617e040980d132bf1265e366a769544ff
Zahra Teymouri <zteymouri@red.software.systems>
2022-01-07 11:49:26 +0100

    eslint and prettier config

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,21 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {CartService} from "../../services/cart.service";
-import {Product} from "../../types/product.type";
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../types/product.type';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
   products: Product[] = [];
   count: number = 0;
   totalPrice: number = 0;
 
-  constructor(
-    private cartService: CartService
-  ) {
-  }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.updateView();
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from "../../services/product.service";
-import {Product} from "../../types/product.type";
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../types/product.type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(
-    private productService: ProductService,
-  ) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.products = this.productService.get();
   }
-
 }
